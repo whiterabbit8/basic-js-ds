57762cb7eb80777dbe97bc4d3697d57b115e686f
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2023-04-16 00:55:27 +0300

    feat: complete queue task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,7 +22,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
+function removeKFromList(l, k) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
