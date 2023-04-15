7391e451e34b04dd71be7e3080e14eacd0e22cee
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2023-04-15 22:13:08 +0300

    feat: complete stack task

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.stack = [];
+  }
+
+  push(element) {
+    return this.stack.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.stack.length > 0) {
+      return this.stack.pop()
+    }
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.stack.length > 0) {
+      return this.stack[this.stack.length - 1]
+    }
   }
 }
 
